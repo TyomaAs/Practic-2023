4fe4578ade18585f0d586804c0a7fb8a33cc4372
TyomaAs <tsipinoartemii@gmail.com>
2023-06-30 00:03:34 +0300

    Organized scroll event

--- Project/js/script.js
+++ Project/js/script.js
@@ -19,3 +19,36 @@ if (iconMenu) {
 		menu.classList.toggle('_active');
 	});
 }
+document.addEventListener('DOMContentLoaded', function () {
+	var hiddenElements = document.querySelectorAll('.hidden');
+	console.log(hiddenElements);
+
+	function isElementInViewport(element) {
+		var rect = element.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	}
+
+	function handleScroll() {
+		hiddenElements.forEach(function (element) {
+			if (isElementInViewport(element)) {
+				element.classList.add('visible');
+			}
+		});
+
+		// Optional: Remove the event listener when all elements are visible
+		if (document.querySelectorAll('.hidden.visible').length === hiddenElements.length) {
+			window.removeEventListener('scroll', handleScroll);
+		}
+	}
+
+	window.addEventListener('scroll', handleScroll);
+
+	// Call handleScroll() once on page load
+	handleScroll();
+});
+
